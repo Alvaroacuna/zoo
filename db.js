ed8fc40a1d19ab755b89f7a3fc58b6e04317f239
Alvaro Acuña <sebastian20.a.m@gmail.com>
2021-02-23 10:11:53 -0300

    Se sube proyecto, con el primer desafío

---
+++ db.js
@@ -0,0 +1,152 @@
+const Sequelize = require('sequelize');
+
+// acá creamos la conexión a la Base de Datos
+const sql = new Sequelize('zoo', 'root', 'LKMachine246', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+const User = sql.define('User', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'Debe indicar un nombre'
+      },
+      len: {
+        args: [2],
+        msg: 'El nombre debe ser de largo al menos 2'
+      }
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'Debe indicar un email'
+      },
+      len: {
+        args: [3],
+        msg: 'El email debe ser de largo al menos 3'
+      },
+      isEmail: {
+        msg: 'Debe ser un email válido'
+      }
+    }
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'Debe indicar una contraseña'
+      },
+      len: {
+        args: [3],
+        msg: 'La contraseña debe ser de largo al menos 3'
+      },
+    }
+  },
+});
+
+
+const Keeper = sql.define('Keeper', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  first_name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'Debe indicar un nombre'
+      },
+      len: {
+        args: [2],
+        msg: 'El nonbre debe ser de largo al menos 2'
+      }
+    }
+  },
+  last_name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'Debe indicar un apellido'
+      },
+      len: {
+        args: [2],
+        msg: 'El apellido debe ser de largo al menos 2'
+      }
+    }
+  },
+  phone: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'Debe indicar un celular'
+      },
+      len: {
+        args: [3],
+        msg: 'El celular debe ser de largo al menos 3'
+      }
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'Debe indicar un email'
+      },
+      len: {
+        args: [3],
+        msg: 'El nonbre debe ser de largo al menos 3'
+      },
+      isEmail: {
+        msg: 'Debe ser un email válido'
+      }
+    }
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'Debe indicar un email'
+      },
+      len: {
+        args: [3],
+        msg: 'El nonbre debe ser de largo al menos 3'
+      },
+    }
+  },
+});
+
+
+//  después sincronizamos nuestro código con la base de datos
+sql.sync()
+.then(() => {
+  console.log('Base de datos y tablas creadas');
+}); 
+
+
+
+
+// finalmente acá listamos todos los modelos que queremos exportar
+module.exports = {
+  Keeper,
+  User
+};
+
+// "password123" => "pbkdf2_sha256$216000$tFUjF5SgzG7T$DFzc9WRagX2wljBU4qcNdDNulR+tprCcFKIlarAUHEk="
---
+++ routes/routes.js
@@ -0,0 +1,41 @@
+/**
+ * Acá dejamos las rutas internas (rutas que asumen que el usuario ya se logueo a nuestra App)
+ */
+const { Router } = require('express');
+const { Keeper } = require('../db');
+const router = Router();
+
+// Middleware: Verifica si el usuario está logueado.
+// en caso de que no, lo mandamos al login
+function checkLogin(req, res, next) {
+  if (req.session.user == null) {
+    return res.redirect('/login');
+  }
+  next();
+}
+
+
+router.get('/', checkLogin, async (req, res) => {
+  res.render('index.ejs');
+});
+
+
+router.get('/dos', checkLogin, async (req, res) => {
+  res.render('dos.ejs');
+});
+
+
+router.post('/', checkLogin, async (req, res) => {
+  try {
+    // acá coloco lo que intento hacer
+    await Keeper.create(req.body);
+
+  } catch (err) {
+    // acá coloco lo que haré si ocurre algún error
+    req.flash('errors', err.errors[key].message);
+  }
+  res.redirect('/');
+});
+
+
+module.exports = router;
